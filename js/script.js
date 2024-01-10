d46477ba091270caa7fd4b14762702f4b87ca588
MichelJJ <82042407+michel-sys@users.noreply.github.com>
2024-01-10 14:35:57 -0300

    1.0

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentSlide = 1;
+
+    function showSlide(index) {
+      document.getElementById(`Slide${index}`).checked = true;
+    }
+
+    function nextSlide() {
+      currentSlide = (currentSlide % 4) + 1;
+      showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 5000);
+  });
+
+
