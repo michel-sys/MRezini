5d8e5c8672e86514656269d624586c828ef2a7cd
MichelJJ <82042407+michel-sys@users.noreply.github.com>
2024-02-08 13:26:41 -0300

    11.3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let currentSlide = 1;
+  let currentSlide = 1;
 
-    function showSlide(index) {
-      document.getElementById(`Slide${index}`).checked = true;
-    }
+  function showSlide(index) {
+    document.getElementById(`Slide${index}`).checked = true;
+  }
 
-    function nextSlide() {
-      currentSlide = (currentSlide % 4) + 1;
-      showSlide(currentSlide);
-    }
+  function nextSlide() {
+    currentSlide = (currentSlide % 4) + 1;
+    showSlide(currentSlide);
+  }
 
-    setInterval(nextSlide, 5000);
-  });
+  setInterval(nextSlide, 5000);
+});
 
 
-  function redirectToTelegram() {
-    window.location.href = "https://t.me/MichelRezini";
+function redirectToTelegram() {
+  window.location.href = "https://t.me/MichelRezini";
 }
