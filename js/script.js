d362cd2ca5e424f81fd121a972e72edb8e5441c6
MichelJJ <82042407+michel-sys@users.noreply.github.com>
2025-01-26 11:42:41 -0300

    1.1

--- js/script.js
+++ js/script.js
@@ -55,19 +55,3 @@ window.onload = function() {
 };
 
 
-
-
-function updateClock() {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-  const timeString = `${hours}:${minutes}:${seconds}`;
-  document.getElementById('clock').textContent = timeString;
-}
-
-// Atualiza o relógio a cada segundo
-setInterval(updateClock, 1000);
-
-// Chama a função uma vez para exibir o horário inicial
-updateClock();
